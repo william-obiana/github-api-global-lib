a2e051470aa05c31c94d9758ab598afe61a4d776
williamobiana <williamobiana@gmail.com>
2023-02-24 16:35:08 +0000

    update

--- vars/buildPythonPackage.groovy
+++ vars/buildPythonPackage.groovy
@@ -41,7 +41,9 @@ def call(String PYTHON_VERSION, String PACKAGE_DIR, String REQUIREMENTS_FILE, St
     }
 
     // create output directory
-    // sh "mkdir ${OUTPUT}"
+    sh "mkdir ${OUTPUT}"
+    sh "cd ${OUTPUT}"
+    sh "ls"
 
     // zip file and create SHA256 hash of the file (this is for lambda to pick up changes)
     sh "zip -r ${OUTPUT}/package.zip ."
